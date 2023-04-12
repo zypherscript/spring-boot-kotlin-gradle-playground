5be8d6979acaa5691b8bce7c39a410b6a5a79754
zypherscript <kitti.kthp@gmail.com>
2023-04-12 17:01:51 +0700

    vendor: upgrade to use spring-boot 3.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.10"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
-    kotlin("plugin.jpa") version "1.6.21"
-    kotlin("plugin.allopen") version "1.6.21"
-    kotlin("kapt") version "1.6.21"
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.spring") version "1.8.20"
+    kotlin("plugin.jpa") version "1.8.20"
+    kotlin("plugin.allopen") version "1.8.20"
+    kotlin("kapt") version "1.8.20"
 }
 
 group = "com.example"
